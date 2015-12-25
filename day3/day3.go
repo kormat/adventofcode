045f6e8400609f4c3bb1c275f016f515a0b02bf4
Stephen Shirley <kormat@gmail.com>
2015-12-25 22:46:06 +0100

    Day 3 part 2.

--- day3/day3.go
+++ day3/day3.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"unicode/utf8"
 )
 
 func main() {
@@ -18,27 +19,50 @@ func main() {
 
 	for scanner.Scan() {
 		dirs := scanner.Text()
-		count := parseDirs(dirs)
-		fmt.Printf("%d houses visited over %d deliveries\n", count, len(dirs)+1)
+		year1 := parseDirsY1(dirs)
+		year2 := parseDirsY2(dirs)
+		fmt.Printf("From %d directions, %d houses were visited in Year 1, %d in Year 2.\n", len(dirs), year1, year2)
 	}
 }
 
-func parseDirs(dirs string) int {
-	houses := make(map[[2]int]int)
+func parseDir(c rune, x, y *int) {
+	switch c {
+	case '^':
+		*y++
+	case 'v':
+		*y--
+	case '<':
+		*x--
+	case '>':
+		*x++
+	}
+}
+
+func parseDirsY1(dirs string) int {
+	houses := make(map[[2]int]bool)
 	x, y := 0, 0
-	houses[[2]int{x, y}] += 1
+	houses[[2]int{x, y}] = true
 	for _, c := range dirs {
-		switch c {
-		case '^':
-			y++
-		case 'v':
-			y--
-		case '<':
-			x--
-		case '>':
-			x++
-		}
-		houses[[2]int{x, y}] += 1
+		parseDir(c, &x, &y)
+		houses[[2]int{x, y}] = true
+	}
+	return len(houses)
+}
+
+func parseDirsY2(dirs string) int {
+	houses := make(map[[2]int]bool)
+	sx, sy := 0, 0
+	rx, ry := 0, 0
+	houses[[2]int{sx, sy}] = true
+	for i := 0; i < len(dirs); {
+		dir, width := utf8.DecodeRuneInString(dirs[i:])
+		parseDir(dir, &sx, &sy)
+		houses[[2]int{sx, sy}] = true
+		i += width
+		dir, width = utf8.DecodeRuneInString(dirs[i:])
+		parseDir(dir, &rx, &ry)
+		houses[[2]int{rx, ry}] = true
+		i += width
 	}
 	return len(houses)
 }
