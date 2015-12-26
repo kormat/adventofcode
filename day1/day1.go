b3c2369fb9e71d1528f0c730290091749bdaf5ef
Stephen Shirley <kormat@gmail.com>
2015-12-26 16:19:38 +0100

    Cleanup day1, add error handling, and input files.

--- day1/day1.go
+++ day1/day1.go
@@ -1,27 +1,57 @@
 package main
 
-import "os"
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	args := os.Args[1]
-	floor, to_minus := calcFloor(args)
-	fmt.Printf("Santa ends up on floor %d, and entered the basement at instruction %d\n", floor, to_minus)
+	lines := readFile()
+	for i, line := range lines {
+		floor, to_minus, err := calcFloor(line)
+		if err {
+			os.Exit(1)
+		}
+		fmt.Printf("%d. Santa ends up on floor %d, and entered the basement at instruction %d\n", i, floor, to_minus)
+	}
 }
 
-func calcFloor(dirs string) (int, int) {
+func readFile() []string {
+	var lines []string
+	if len(os.Args) != 2 {
+		log.Fatal("Expecting a single input file as argument")
+	}
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
+
+func calcFloor(dirs string) (int, int, bool) {
 	floor := 0
 	to_minus := -1
 	for i, c := range dirs {
-		if c == '(' {
+		switch c {
+		case '(':
 			floor++
-		} else if c == ')' {
+		case ')':
 			floor--
+		default:
+			log.Printf("Unrecognised instruction '%c' at index %d", c, i)
+			return 0, 0, true
 		}
-		if floor < 0 && to_minus == -1 {
+		if to_minus == -1 && floor < 0 {
 			to_minus = i + 1
 		}
-		fmt.Printf("Idx: %d Char: %c Floor: %d To minus: %d\n", i, c, floor, to_minus)
 	}
-	return floor, to_minus
+	return floor, to_minus, false
 }
