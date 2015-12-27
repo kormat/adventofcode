2be3c4eb839d55a2d8332c84c8e680f5becd4aff
Stephen Shirley <kormat@gmail.com>
2015-12-27 14:07:28 +0100

    Use channels for returning results in day4

--- day4/day4.go
+++ day4/day4.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"container/list"
 	"crypto/md5"
 	"fmt"
 	"github.com/kormat/adventofcode/util"
 	"os"
 )
+
+type Result struct {
+	prefixLen int
+	coin      string
+	idx       int
+}
 
 func main() {
 	lines, err := util.ReadFileArg()
@@ -14,19 +21,30 @@ func main() {
 	}
 
 	for i, key := range lines {
-		coin, idx := mineCoin(key)
-		fmt.Printf("%d. Key: %s. Coin: %s Coin idx: %d\n", i, key, coin, idx)
+		c := make(chan Result)
+		go mineCoin(key, c)
+		fmt.Printf("%d. Key: %s.\n", i, key)
+		for res := range c {
+			fmt.Printf("  Prefix len: %d Coin: %s Coin idx: %d\n", res.prefixLen, res.coin, res.idx)
+		}
 	}
 }
 
-func mineCoin(key string) (string, int) {
-	target := "000000"
-	for i := 0; ; i++ {
+func mineCoin(key string, c chan Result) {
+	targets := list.New()
+	targets.PushFront("00000")
+	targets.PushFront("000000")
+	for i := 0; targets.Len() > 0; i++ {
 		full_key := fmt.Sprintf("%s%d", key, i)
 		byte_hash := md5.Sum([]byte(full_key))
 		hash := fmt.Sprintf("%x", byte_hash)
-		if hash[:len(target)] == target {
-			return hash, i
+		for t := targets.Front(); t != nil; t = t.Next() {
+			tlen := len(t.Value.(string))
+			if hash[:tlen] == t.Value {
+				c <- Result{tlen, hash, i}
+				targets.Remove(t)
+			}
 		}
 	}
+	close(c)
 }
