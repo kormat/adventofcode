5ea16a4adbb976f4d5f995c8f4f9f7d52fea2c7c
Stephen Shirley <kormat@gmail.com>
2015-12-29 15:19:12 +0100

    Day 5, part 2 complete.
    Split day 5 into two dirs.

---
+++ day5p2/day5p2.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kormat/adventofcode/util"
+	"os"
+)
+
+func main() {
+	lines, err := util.ReadFileArg()
+	if err {
+		os.Exit(1)
+	}
+	nice := 0
+	naughty := 0
+	for i, word := range lines {
+		result := isStringNice(word)
+		if result {
+			nice++
+		} else {
+			naughty++
+		}
+		fmt.Printf("%d. %s: %v\n", i, word, result)
+	}
+	fmt.Printf("Summary: %d words, %d nice, %d naughty\n", len(lines), nice, naughty)
+}
+
+func isStringNice(chars string) bool {
+	if hasValidPairs(chars) && hasValidRepeat(chars) {
+		return true
+	}
+	return false
+}
+
+func hasValidPairs(chars string) bool {
+	pairs := make(map[string][]int)
+	for i := 0; i < len(chars)-1; i++ {
+		pair := chars[i : i+2]
+		pairs[pair] = append(pairs[pair], i)
+	}
+	for _, idxs := range pairs {
+		if len(idxs) < 2 {
+			continue
+		}
+		first := idxs[0]
+		for _, second := range idxs[1:] {
+			if second > first+1 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func hasValidRepeat(chars string) bool {
+	for i := 0; i < len(chars)-2; i++ {
+		if chars[i] == chars[i+2] {
+			return true
+		}
+	}
+	return false
+}
