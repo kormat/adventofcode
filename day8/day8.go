b8d3468623751c0a805d7ecbb455242c8cb9acad
Stephen Shirley <kormat@gmail.com>
2016-01-01 17:13:20 +0100

    Day 8 complete.

---
+++ day8/day8.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kormat/adventofcode/util"
+	"os"
+	"strings"
+)
+
+func main() {
+	lines, err := util.ReadFileArg()
+	if err {
+		os.Exit(1)
+	}
+	var total_lit, total_mem, total_reenc int
+	for _, line := range lines {
+		total_lit += len(line)
+		total_mem += countEscaped(line)
+		total_reenc += countReencoded(line)
+	}
+	fmt.Printf("Lines: %d Literals: %d\n", len(lines), total_lit)
+	fmt.Printf("Memory: %d Literal overhead: %d\n", total_mem, total_lit-total_mem)
+	fmt.Printf("Reencoded: %d Overhead: %d\n", total_reenc, total_reenc-total_lit)
+}
+
+func countEscaped(line string) int {
+	var chars int
+	for i := 1; i < len(line)-1; {
+		s := line[i:]
+		switch {
+		case strings.HasPrefix(s, `\\`), strings.HasPrefix(s, `\"`):
+			chars++
+			i += 2
+		case strings.HasPrefix(s, `\x`):
+			chars++
+			i += 4
+		default:
+			chars++
+			i += 1
+		}
+	}
+	return chars
+}
+
+func countReencoded(line string) int {
+	chars := 3 // Opening "\"
+	for i := 1; i < len(line)-1; i++ {
+		switch line[i] {
+		case '"', '\\': // Become \" and \\ respectively
+			chars += 2
+		default:
+			chars += 1
+		}
+	}
+	chars += 3 // Closing \""
+	return chars
+}
